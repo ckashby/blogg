88598cafb72547374298fa098fbd34f6f92f1694
ckashby <clay.ashby@gmail.com>
2018-06-09 17:38:52 -0700

    changed users_new_path to get 'signup', to: 'users#new', updated test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   root 'pages#index'
   get 'index', to: 'pages#index'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
